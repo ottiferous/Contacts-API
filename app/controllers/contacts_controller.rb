79a5e8c2f6f8de98318eb3269b62fc3d63b0900f
App Academy Student <student@appacademy.io>
2014-02-18 15:27:58 -0800

    initialized the Contact Controller incorrectly. This has been fixed"

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,44 @@
+class ContactsController < ApplicationController
+  #resources :contacts
+
+  def index
+    contact = Contact.all
+    render json: contact
+  end
+
+  def show
+    contact = Contact.find(params[:id])
+    render json: => contact
+  end
+
+  def create
+    contact = Contact.new(contact_params)
+    if contact.save
+      render json: contact
+    else
+      render json: contact.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    contact = Contact.find(params[:id])
+    if contact.update_attributes(contact_params)
+      render json: contact
+    else
+      render json: contact.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    contact = Contact.find(params[:id])
+    contact.delete
+    render json: contact
+  end
+
+  private
+
+  def contact_params
+    params.permit(:name, :email, :user_id)
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 SampleApp::Application.routes.draw do
   # resources :users
+
   resources :authorize, only: [:create, :destroy, :index, :show, :update]
 
+  #User Routes
+  get     '/'     => 'users#index'
+  get     'index' => 'users#index'
   get     'users' => 'users#index'
   post    'users' => 'users#create'
   get     'users/new' => 'users#new'
@@ -10,4 +14,15 @@ SampleApp::Application.routes.draw do
   patch   'users/:id' => 'users#update'
   put     'users/:id' => 'users#update'
   delete  'users/:id' => 'users#destroy'
+
+  #Contact Routes
+  get     'contact' => 'contact#index'
+  post    'contact' => 'contact#create'
+  get     'contact/new' => 'contact#new'
+  get     'contact/:id/edit' => 'user#edit'
+  get     'contact/:id' => 'contact#show'
+  patch   'contact/:id' => 'contact#update'
+  put     'contact/:id' => 'contact#update'
+  delete  'contact/:id' => 'contact#destroy'
+
 end
