25058c717aa26ab433781a92843c30be5beda302
App Academy Student <student@appacademy.io>
2014-02-18 17:50:49 -0800

    cleaned up the routing stuffs

--- config/routes.rb
+++ config/routes.rb
@@ -4,48 +4,21 @@ SampleApp::Application.routes.draw do
   resources :authorize, only: [:create, :destroy, :index, :show, :update]
 
   # Creates the /users/:id/contacts setup without all that typing.
-  resources :users do
+  resources :users, except: [:edit, :new] do
     get 'index', on: :member
   end
 
-  # Working on this
-  resources :contacts
+  resources :contacts, except: [:edit, :new]
 
   get     'users/:user_id/contacts' =>  'contacts#index'
 
   resources :contactshares do
     member do
-      post 'contactshare/new'
-      delete 'contactshare/:id'
+      post 'contactshare/new', :to => 'contactshare#new'
+      delete 'contactshare/:id', :to => 'contactshare#destroy'
     end
   end
 
-  #User Routes
-  get     '/'     => 'users#index'
-  get     'index' => 'users#index'
-  get     'users' => 'users#index'
-  post    'users' => 'users#create'
-  get     'users/new' => 'users#new'
-  get     'users/:id/edit' => 'user#edit'
-  get     'users/:id' => 'users#show'
-  patch   'users/:id' => 'users#update'
-  put     'users/:id' => 'users#update'
-  delete  'users/:id' => 'users#destroy'
+  root :to => 'users#index'
 
-  #Nested Route(s)
-
-
-  #Contact Routes
-  # get     'contacts' => 'contacts#index'
-  # post    'contacts' => 'contacts#create'
-  # get     'contacts/new' => 'contacts#new'
-  # get     'contacts/:id/edit' => 'contacts#edit'
-  # get     'contacts/:id' => 'contacts#show'
-  # patch   'contacts/:id' => 'contacts#update'
-  # put     'contacts/:id' => 'contacts#update'
-  # delete  'contacts/:id' => 'contacts#destroy'
-
-  #ContactShare Routes
-  # post    'contactshare/new' => 'contactshare#create'
-  # delete  'contactshare/:id' => 'contactshare#destroy'
 end
